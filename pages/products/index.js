76e37c9ff0ebfd81e9b751f1c06f78eb19ee21be
Federico Tejedor Llorente <federicotllorente@gmail.com>
2021-05-30 22:11:40 +0200

    Product list page updated: now the filters by brand and type are working properly

--- pages/products/index.js
+++ pages/products/index.js
@@ -1,19 +1,14 @@
 import { useState, useEffect, useRef } from 'react'
-import useFetchProducts from '../../hooks/useFetchProducts'
 import useHandleFilters from '../../hooks/useHandleFilters'
 import useIsMobile from '../../hooks/useIsMobile'
 
 import FiltersModal from '../../components/FiltersModal'
 import ProductCard from '../../components/ProductCard'
-import { SearchError } from '../../components/ErrorMessages'
-import { LoaderPage } from '../../components/Loader'
 
 const productsAPI = 'http://makeup-api.herokuapp.com/api/v1/products.json'
 // const productsAPI = 'http://localhost:3000/api/products'
 
-const ProductList = () => {
-	const [currentPage, setCurrentPage] = useState(0)
-	const productsPerPage = 30
+const ProductList = ({ products }) => {
 	const isTablet = useIsMobile(800)
 	const elementRef = useRef()
 
@@ -23,50 +18,85 @@ const ProductList = () => {
 		handleBrandFilterInput
 	} = useHandleFilters()
 
-	const {
-		fetchProducts, products,
-		loadingProducts, errorProducts
-	} = useFetchProducts()
+	const [filteredProducts, setFilteredProducts] = useState([])
+
+	const filterByType = () => {
+		const newFilteredProductsArray = products.filter(product => (
+			product.product_type === appliedFilters.type.query_param_name)
+		)
+		setFilteredProducts(newFilteredProductsArray)
+	}
+
+	const filterByBrand = () => {
+		const newFilteredProductsArray = products.filter(product => (
+			product.brand === appliedFilters.brand.name
+		))
+		setFilteredProducts(newFilteredProductsArray)
+	}
+
+	const filterByTypeAndBrand = () => {
+		const newFilteredProductsArray = products.filter(product => (
+			product.product_type === appliedFilters.type.query_param_name &&
+			product.brand === appliedFilters.brand.name
+		))
+		setFilteredProducts(newFilteredProductsArray)
+	}
+
+	let typeFilterSelected = appliedFilters.type.query_param_name
+	let brandFilterSelected = appliedFilters.brand.query_param_name
+	let filterSelected = typeFilterSelected || brandFilterSelected
 
 	useEffect(() => {
-		fetchProducts(productsAPI)
-		products && setCurrentPage(1)
-	}, [])
-
-	errorProducts && <SearchError error={errorProducts} />
+		if (products) {
+			if (!typeFilterSelected && !brandFilterSelected) setFilteredProducts([])
+			if (typeFilterSelected && !brandFilterSelected) filterByType()
+			if (!typeFilterSelected && brandFilterSelected) filterByBrand()
+			if (typeFilterSelected && brandFilterSelected) filterByTypeAndBrand()
+		}
+	}, [appliedFilters])
 
 	return (
-		loadingProducts
-			? <LoaderPage />
-			: <main className="product_list">
-				<h2>All products</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac laoreet urna, a suscipit lacus. Ut quis nisl ex. Nunc condimentum arcu sem, at vehicula velit fringilla a.</p>
-				<FiltersModal
-					appliedFilters={appliedFilters}
-					handleTypeFilterInput={handleTypeFilterInput}
-					handleBrandFilterInput={handleBrandFilterInput}
-					isTablet={isTablet}
-				/>
-				<div className="product_list__container">
-					{products?.slice(0, productsPerPage * currentPage).map((product, index, array) => {
-						return (
-							<ProductCard
-								key={product.id}
-								product={product}
-								elementRef={index == array.length - 1 ? elementRef : null}
-							/>
-						)
-					})}
+		<main className="product_list">
+			<h2>All products</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus ac laoreet urna, a suscipit lacus. Ut quis nisl ex. Nunc condimentum arcu sem, at vehicula velit fringilla a.</p>
+			<FiltersModal
+				appliedFilters={appliedFilters}
+				handleTypeFilterInput={handleTypeFilterInput}
+				handleBrandFilterInput={handleBrandFilterInput}
+				isTablet={isTablet}
+			/>
+			<div className="product_list__container">
+				{!filterSelected && products?.map((product, index, array) => (
+					<ProductCard
+						key={product.id}
+						product={product}
+						elementRef={index == array.length - 1 ? elementRef : null}
+					/>
+				))}
+				{filterSelected && filteredProducts?.map((product, index, array) => (
+					<ProductCard
+						key={product.id}
+						product={product}
+						elementRef={index == array.length - 1 ? elementRef : null}
+					/>
+				))}
+			</div>
+			{(filterSelected && filteredProducts.length == 0) && (
+				<div className="product_list__no_products">
+					<hr />
+					<h2>There&apos;s no products to show</h2>
+					<p>Please try with other filters</p>
 				</div>
-			</main>
+			)}
+		</main>
 	)
 }
 
 export const getServerSideProps = async context => {
 	const response = await fetch(productsAPI)
 	if (response.ok) {
-		const props = await response.json()
-		return { props: { props } }
+		const products = await response.json()
+		return { props: { products } }
 	}
 	if (context.res) {
 		// This will show a 404 error
